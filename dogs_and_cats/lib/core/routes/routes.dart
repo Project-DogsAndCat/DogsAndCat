28a95a67eca5b448a204a603f790184b2b97042f
Vadim <lyamin.vadim.05@mail.ru>
2025-03-06 22:56:51 +0700

    Реализовал возможность управления сессиями

--- dogs_and_cats/lib/core/routes/routes.dart
+++ dogs_and_cats/lib/core/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:dogs_and_cats/core/dependency/dependencies.dart';
 import 'package:dogs_and_cats/core/routes/route_names.dart';
 import 'package:dogs_and_cats/presentation/account/pages/profile_page.dart';
 import 'package:dogs_and_cats/presentation/auth/pages/login_page.dart';
@@ -7,49 +8,54 @@ import 'package:dogs_and_cats/presentation/scaffold_with_navbar/pages/scaffold_w
 import 'package:dogs_and_cats/presentation/search/pages/search_page.dart';
 import 'package:dogs_and_cats/presentation/settings/page/settings_page.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-final GoRouter router = GoRouter(routes: [
-  GoRoute(
-      name: RoutesNames.register,
-      path: '/',
-      builder: (context, state) => const RegisterPage()),
-  GoRoute(
-      name: RoutesNames.login,
-      path: '/login',
-      builder: (context, state) => const LoginPage()),
-  StatefulShellRoute.indexedStack(
-      builder: (context, state, navigationShell) =>
-          ScaffoldWithNavbar(navigationShell: navigationShell),
-      branches: [
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              name: RoutesNames.search,
-              path: '/search',
-              builder: (context, state) => const SearchPage(),
+final session = getIt<SupabaseClient>().auth.currentSession;
+
+final GoRouter router = GoRouter(
+    initialLocation: session == null ? '/register' : '/search',
+    routes: [
+      GoRoute(
+          name: RoutesNames.register,
+          path: '/register',
+          builder: (context, state) => const RegisterPage()),
+      GoRoute(
+          name: RoutesNames.login,
+          path: '/login',
+          builder: (context, state) => const LoginPage()),
+      StatefulShellRoute.indexedStack(
+          builder: (context, state, navigationShell) =>
+              ScaffoldWithNavbar(navigationShell: navigationShell),
+          branches: [
+            StatefulShellBranch(
+              routes: [
+                GoRoute(
+                  name: RoutesNames.search,
+                  path: '/search',
+                  builder: (context, state) => const SearchPage(),
+                ),
+              ],
             ),
-          ],
-        ),
-        StatefulShellBranch(routes: [
-          GoRoute(
-            name: RoutesNames.dogsitter,
-            path: '/dogsitter_page',
-            builder: (context, state) => const DogsitterPage(),
-          ),
-        ]),
-        StatefulShellBranch(routes: [
-          GoRoute(
-            name: RoutesNames.account,
-            path: '/profile',
-            builder: (context, state) => const ProfilePage(),
-          )
-        ]),
-        StatefulShellBranch(routes: [
-          GoRoute(
-            name: RoutesNames.settings,
-            path: '/settings',
-            builder: (context, state) => const SettingsPage(),
-          )
-        ]),
-      ]),
-]);
+            StatefulShellBranch(routes: [
+              GoRoute(
+                name: RoutesNames.dogsitter,
+                path: '/dogsitter_page',
+                builder: (context, state) => const DogsitterPage(),
+              ),
+            ]),
+            StatefulShellBranch(routes: [
+              GoRoute(
+                name: RoutesNames.account,
+                path: '/profile',
+                builder: (context, state) => const ProfilePage(),
+              )
+            ]),
+            StatefulShellBranch(routes: [
+              GoRoute(
+                name: RoutesNames.settings,
+                path: '/settings',
+                builder: (context, state) => const SettingsPage(),
+              )
+            ]),
+          ]),
+    ]);
